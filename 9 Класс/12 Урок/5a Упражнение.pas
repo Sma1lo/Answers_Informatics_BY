55a0625150f44bfc2f242ba147ba9db56fe79ae9
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-06 19:53:09 +0300

    5a Упражнение.pas

--- 9 Класс/12 Урок/5a Упражнение.pas
+++ 9 Класс/12 Урок/5a Упражнение.pas
@@ -1,1 +1,22 @@
-s
+program CountWords;
+
+var
+  s: string;
+  w, i: integer;
+
+begin
+  w := 1; 
+  writeln('Введите предложение:');
+  readln(s);
+
+  
+  for i := 1 to length(s) do
+  begin
+    if s[i] = ' ' then
+      w := w + 1;
+  end;
+
+  
+  writeln('Количество слов в предложении: ', w);
+
+end.
