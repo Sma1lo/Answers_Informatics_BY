471658601b386a88a6b6b2b6f3804ca8d6170aa9
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-06 20:41:33 +0300

    6б Упражнение.pas

--- 9 Класс/12 Урок/6б Упражнение.pas
+++ 9 Класс/12 Урок/6б Упражнение.pas
@@ -1,1 +1,20 @@
-b
+var s,sc: string;
+    i,k: integer;
+begin
+write('Введите строку: ');
+readln(s);
+for i:=1 to length(s)-1 do
+    begin
+    sc:=copy(s,i,2);
+    if (sc='жы') or (sc='шы') then
+       begin
+       s[i+1]:='и';
+       inc(k);
+       end;
+    end;
+if k=0 then writeln('Ошибок не обнаружено') else
+begin
+writeln('Обнаружены ошибки в количестве ',k,'. Ошибки исправлены');
+writeln(s);
+end;
+end.
