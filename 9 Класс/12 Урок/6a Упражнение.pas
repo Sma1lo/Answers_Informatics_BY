cae0928f175681f848bc659342559ed0868c1f11
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-06 20:29:56 +0300

    6a Упражнение.pas

--- 9 Класс/12 Урок/6a Упражнение.pas
+++ 9 Класс/12 Урок/6a Упражнение.pas
@@ -1,1 +1,14 @@
-h
+var s: string; i, m: integer;
+begin
+write('Введите строку: ');
+read(s);
+
+for i:=1 to length(s) - 1 do
+if ((s[i] = 'Ч') or (s[i] = 'ч')) and ((s[i + 1] = 'Я') or (s[i + 1] = 'я')) then begin
+inc(m, 1);
+s[i + 1] := 'а';
+end;
+
+writeln('Ошибок: ',m);
+writeln(s);
+end.
