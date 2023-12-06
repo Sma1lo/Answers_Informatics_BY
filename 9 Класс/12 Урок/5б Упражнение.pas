f2ec55583020e6a52ffb3f3b23bbfdc48f099103
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-06 19:56:56 +0300

    Update 5б Упражнение.pas

--- 9 Класс/12 Урок/5б Упражнение.pas
+++ 9 Класс/12 Урок/5б Упражнение.pas
@@ -1,1 +1,31 @@
-s
+var
+  s: string;
+  w, i: integer;
+  c: boolean;
+
+begin
+  w := 0;
+  c := false;
+
+  writeln('Введите предложение:');
+  readln(s);
+
+  for i := 1 to length(s) do
+  begin
+    if s[i] <> ' ' then
+    begin
+      if not c then
+      begin
+        c := true;
+        w := w + 1;
+      end;
+    end
+    else
+    begin
+      c := false;
+    end;
+  end;
+
+  writeln('Количество слов в предложении: ', w);
+
+end.
