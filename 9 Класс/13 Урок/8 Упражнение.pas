9c97edae08870a7614b5e0e71e44e2a60d8a8b32
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-13 20:05:29 +0300

    Update 8 Упражнение.pas

--- 9 Класс/13 Урок/8 Упражнение.pas
+++ 9 Класс/13 Урок/8 Упражнение.pas
@@ -1,1 +1,22 @@
-ы
+var
+  s: string;
+  c, i: integer;
+  a: set of char;
+
+begin
+  a := ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+  c := 0;
+  
+  writeln('Введите предложение: ');
+  readln(s);
+  
+  for i := 1 to length(s) do
+  begin
+    if s[i] in a then
+    begin
+      Inc(c);
+    end;
+  end;
+
+  writeln('Количество гласных букв в предложении: ', c);
+end.
