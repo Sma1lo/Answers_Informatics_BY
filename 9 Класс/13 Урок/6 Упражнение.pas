d33087afbd0a931e98ae695347513602bf2c4868
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-13 19:47:10 +0300

    Update 6 Упражнение.pas

--- 9 Класс/13 Урок/6 Упражнение.pas
+++ 9 Класс/13 Урок/6 Упражнение.pas
@@ -1,1 +1,22 @@
-s
+var
+  a, b, c, d, e: string;
+  p: integer;
+begin
+ 
+  Write('Введите дату в американском формате (м/д/гггг): ');
+  ReadLn(a);
+
+  
+  p := Pos('/', a);
+  c := Copy(a, 1, p - 1);
+  Delete(a, 1, p);
+  p := Pos('/', a);
+  d := Copy(a, 1, p - 1);
+  Delete(a, 1, p);
+  e := a;
+
+    b := d + '.' + c + '.' + e;
+
+  
+  WriteLn('Дата в формате, принятом в Беларуси: ', b);
+end.
