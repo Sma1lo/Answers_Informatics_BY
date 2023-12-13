a2716527808cfe6c64b758e179ae6097c4815dfc
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-13 19:39:25 +0300

    Update 5 Упражнение.pas

--- 9 Класс/13 Урок/5 Упражнение.pas
+++ 9 Класс/13 Урок/5 Упражнение.pas
@@ -1,1 +1,29 @@
-e
+var
+  a, b: string;
+  i: integer;
+
+begin
+  writeln('Введите строку:');
+  readln(a);
+
+  b := '';
+  for i := 1 to length(a) do
+  begin
+    if (i = 1) or ((i > 1) and (a[i - 1] = '.')) then
+    begin
+      b := b + UpCase(a[i]);
+    end
+    else
+    begin
+      b := b + a[i];
+    end;
+
+    if (a[i] = '.') then
+    begin
+      b := b + ' ';
+    end;
+  end;
+
+  writeln('Результат:');
+  writeln(b);
+end.
