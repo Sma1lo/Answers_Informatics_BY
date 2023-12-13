0646bbd3cbf22e417b4d2876523c46262a6ee3f2
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-13 20:01:05 +0300

    Update 7 Упражнение.pas

--- 9 Класс/13 Урок/7 Упражнение.pas
+++ 9 Класс/13 Урок/7 Упражнение.pas
@@ -1,1 +1,25 @@
-s
+var
+  n, f: string;
+  i, j, l: integer;
+
+begin
+  writeln('Введите число: ');
+  readln(n);
+  
+  l := length(n);
+  j := 0;
+  f := '';
+
+  for i := l downto 1 do
+  begin
+    f := n[i] + f;
+    Inc(j);
+    if (j = 3) and (i > 1) then
+    begin
+      f := ' ' + f;
+      j := 0;
+    end;
+  end;
+
+  writeln('Результат: ', f);
+end.
