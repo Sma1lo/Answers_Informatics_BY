369f4ffdab57b5b9e70dc460f95d33091cff8fe3
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-13 19:24:40 +0300

    Update 4 Упражнение.pas

--- 9 Класс/13 Урок/4 Упражнение.pas
+++ 9 Класс/13 Урок/4 Упражнение.pas
@@ -1,1 +1,22 @@
-s
+var
+  a, b: string;
+  i: integer;
+
+begin
+  writeln('Введите строку:');
+  readln(a);
+
+ b := '';
+  for i := 1 to length(a) do
+  begin
+    if (a[i] = ' ') then
+      b := b + Chr(183)
+    else
+      b := b + a[i];
+  end;
+
+  b := b + Chr(182);
+
+  writeln('Результат:');
+  writeln(b);
+end.
