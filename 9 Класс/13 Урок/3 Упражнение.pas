73c5e890f965fc9459ecfca3b2538845c610ff4f
Sma1lo <135868190+Sma1lo@users.noreply.github.com>
2023-12-13 19:18:13 +0300

    Update 3 Упражнение.pas

--- 9 Класс/13 Урок/3 Упражнение.pas
+++ 9 Класс/13 Урок/3 Упражнение.pas
@@ -1,1 +1,25 @@
-s
+var
+  a, b: string;
+  i: integer;
+
+begin
+  writeln('Введите строку: ');
+  readln(a);
+
+  b := '';
+  i := 1;
+  while i <= length(a) do
+  begin
+    if (a[i] = ' ') and ((i = 1) or (i = length(a)) or (a[i + 1] = ' ') or (a[i + 1] in ['.', ',', '!', '?', ';', ':'])) then
+    begin
+      i := i + 1;
+    end
+    else
+    begin
+      b := b + a[i];
+      i := i + 1;
+    end;
+  end;
+  
+  writeln('Результат: ', b);
+end.
